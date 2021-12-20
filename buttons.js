51f12589361813d9c3ef7fbd3fbcf55863b88094
Matts ZOoety <emzeegotanemail2@gmail.com>
2021-12-20 12:01:22 -0500

    Upgrade rotate button

--- buttons.js
+++ buttons.js
@@ -1,14 +1,34 @@
-const webcamVideoElement = document.getElementsByClassName('new_input_video')[0];
-const outputCanvasElement = document.getElementsByClassName('output_canvas')[0];
-const visionContainerElement = document.getElementsByClassName('vision-container')[0];
+const elementsToRotate = document.getElementsByClassName("element-to-rotate");
+const rotationWrapperElements = document.getElementsByClassName("rotation-wrapper");
 const rotateButtonElement = document.getElementById('rotate_button');
 
 let isPortrait = false;
 function handleRotateButton() {
-    isPortrait = !isPortrait;
+    console.log("clicked button!");
 
-    if (isPortrait) {
-        visionContainerElement.style.transform = "rotate(90deg)  translate(0px, -730px)";
+    for (let i = 0; i < elementsToRotate.length; i++) {
+        const currElementToRotate = elementsToRotate[i];
+        const currRotationWrapperElement = rotationWrapperElements[i];
+    
+        const currElementWidth = currElementToRotate.offsetWidth;
+        const currElementHeight = currElementToRotate.offsetHeight;
+    
+        if (isPortrait) {
+          currRotationWrapperElement.style.width = `${currElementWidth}px`;
+          currRotationWrapperElement.style.height = `${currElementHeight}px`;
+          currElementToRotate.classList.remove("rotate-me");
+        } else{
+          currRotationWrapperElement.style.width = `${currElementHeight}px`;
+          currRotationWrapperElement.style.height = `${currElementWidth}px`;      
+          currElementToRotate.classList.add("rotate-me");
+        }
+    
+        console.log(`Box Offset Width: ${currElementToRotate.offsetWidth}`);
+        console.log(`Box Offset Height: ${currElementToRotate.offsetHeight}`);
+        console.log("=================");
     }
+    
+    isPortrait = !isPortrait;
 }
+
 rotateButtonElement.addEventListener("click", handleRotateButton);
