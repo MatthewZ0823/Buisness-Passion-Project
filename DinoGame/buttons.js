399d3e216ba089b0883e9d2d6826fa40397a4234
Matts ZOoety <emzeegotanemail2@gmail.com>
2022-01-10 22:25:05 -0500

    Copy over files

---
+++ DinoGame/buttons.js
@@ -0,0 +1,35 @@
+const elementsToRotate = document.getElementsByClassName("element-to-rotate");
+const rotationWrapperElements = document.getElementsByClassName("rotation-wrapper");
+const rotateButtonElement = document.getElementById('rotate_button');
+
+let isPortrait = false;
+function handleRotateButton() {
+    console.log("clicked button!");
+
+    for (let i = 0; i < elementsToRotate.length; i++) {
+        const currElementToRotate = elementsToRotate[i];
+        const currRotationWrapperElement = rotationWrapperElements[i];
+    
+        const currElementWidth = currElementToRotate.offsetWidth;
+        const currElementHeight = currElementToRotate.offsetHeight;
+    
+        if (isPortrait) {
+          currRotationWrapperElement.style.width = `${currElementWidth}px`;
+          currRotationWrapperElement.style.height = `${currElementHeight}px`;
+          currElementToRotate.classList.remove("rotate-me");
+        } else {
+          currRotationWrapperElement.style.width = `${currElementHeight}px`;
+          currRotationWrapperElement.style.height = `${currElementWidth}px`;      
+          currElementToRotate.classList.add("rotate-me");
+        }
+    
+        // console.log(currElementToRotate);
+        // console.log(`Box Offset Width: ${currElementToRotate.offsetWidth}`);
+        // console.log(`Box Offset Height: ${currElementToRotate.offsetHeight}`);
+        // console.log("=================");
+    }
+    
+    isPortrait = !isPortrait;
+}
+
+rotateButtonElement.addEventListener("click", handleRotateButton);
---
+++ DinoGame/constants.js
@@ -0,0 +1,13 @@
+const jointLandmarkNumbers = {
+    leftShoulder: 11,
+    rightShoulder: 12,
+    leftElbow: 13,
+    rightElbow: 14, 
+    leftWaist: 23,
+    rightWaist: 24,
+    leftKnee: 25,
+    rightKnee: 26,
+};
+
+const videoWidth = 640;
+const videoHeight = 480;
